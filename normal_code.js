3772a92bd12dd3077e7dceb89fd3c9d5fb2d992b
sourav2808 <souravranjanpanda28@gmail.com>
2024-12-25 17:14:45 +0530

    Updated code

--- normal_code.js
+++ normal_code.js
@@ -1,2 +1,19 @@
+//number Data Type: All Numbers (whole, decimal, positive, negative)
 var a = 456/0;
 console.log(a, typeof a);
+
+//strings: Single character or series of characters all are strings
+var b = 'Kodnest'
+console.log(b, typeof b)
+
+//Boolean: true / false
+var c = true;
+console.log(c, typeof c)
+
+//undefined:
+let d;
+console.log(d); //undefined
+
+//null:
+let e = null;
+console.log(e); //null
